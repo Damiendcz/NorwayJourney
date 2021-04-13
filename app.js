1773dc7f31b7db3da220830d6e9883b410ec467b
DamienDcz <damiendescroizette@gmail.com>
2021-04-14 00:04:48 +0200

    ajout d'animation gsap

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log(gsap);
+gsap.from('#title', {opacity: 0, y: 50, duration: 1})
+gsap.from('#cta', {opacity: 0, y: 50, duration: 1, delay: .5})
+gsap.to('.intro', {opacity : 0, duration: 2})
+gsap.from('.experience', {x: 500, duration: 2, opacity: 0})
